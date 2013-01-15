808723820d6c7b4791bc37e8d208c1d6f38ed810
rossputin <ross@bheap.co.uk>
2013-01-15 16:57:24 +0000

    require lein 2.0.0

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject silk "0.1.0-SNAPSHOT"
   :description "Silk static and dynamic publishing toolkit."
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]]
 
   :aot :all)
