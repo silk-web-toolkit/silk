7a6e67db025e2ff4f389c43c5829e0c45831d5e3
rossputin <ross@bheap.co.uk>
2013-01-18 21:49:02 +0000

    add validate pipeline transformers and input

---
+++ src/silk/transform/validate.clj
@@ -0,0 +1,44 @@
+(ns silk.transform.validate
+  "TODO: this is really an import validate pipeline, rename/reorg as such.
+     seed-data, structural-analysis and field-analysis are more generic but
+     tx-eligibility-analysis is fairly specific to an import.
+   Validation related transformations.
+   Note our payload is typically something like validate-payload
+   see input.pipeline.")
+
+;; =============================================================================
+;; Validate transformation functions, see namespace comment
+;; =============================================================================
+
+(defn seed-data->
+  [d m]
+  (assoc m :seed-data d :seed-data-count (count d)))
+
+;;(every? #(= 8 (count %)) data)
+(defn structural-precog->
+  "Validate structural integrity of seed-data according to a predicate p.
+   Yields :invalid or :valid."
+  [p m]
+  (when p (assoc m :structural-precog :valid)))
+
+(defn field-precog->
+  "Validate specific data fields, partitioning given a function fn.
+   Reduces :seed-data accordingly upon finding invalid entries.
+   Yields :invalid, :partially-valid or :valid."
+  [fn m]
+  (when (= :valid (:structural-precog m))
+    (let [fn-val (fn (:seed-data m))]
+      (cond
+        (empty? (last fn-val)) (assoc m :field-precog :valid)
+        (empty? (first fn-val)) m
+        (and (> (count (first fn-val)) 0) (> (count (last fn-val)) 0))
+          (assoc m :field-precog :partially-valid
+                   :seed-data (first fn-val)
+                   :invalid-field-data (last fn-val))))))
+
+;;(defn tx-eligibility-precog->
+  ;;"Validate if the system should accept the data.
+   ;;Yields :invalid, :partially-valid or :valid."
+;;[ m]
+;;(when-not (= :invalid (:field-precog m))
+  ;;)
