a44565f5c68985bc2a4978e115cb571ff75f5b4f
rossputin <ross@bheap.co.uk>
2013-01-20 21:23:55 +0000

    remove defunt TODO for renaming ns

--- src/silk/input/pipeline.clj
+++ src/silk/input/pipeline.clj
@@ -1,6 +1,5 @@
 (ns silk.input.pipeline
-  "TODO : rename ns to something better... (webapp ?)
-   Generic inputs (payloads) within a transformation pipeline.")
+  "Generic inputs (payloads) within a transformation pipeline.")
 
 (def pipe-data {:data []
                 :page {:offset nil :limit nil :total nil}
