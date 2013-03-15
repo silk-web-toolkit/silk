d70276c85a0366ba3a589a9b764af59483bc828c
rossputin <ross@bheap.co.uk>
2013-03-15 15:10:45 +0000

    timestamp snapshot for 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk "0.1.0-alpha.2-SNAPSHOT"
+(defproject silk "0.1.0-20130315-150900"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
