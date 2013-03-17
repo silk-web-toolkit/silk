4a4264e4e081d721a8884d52237f02ee64e512de
rossputin <ross@bheap.co.uk>
2013-03-17 15:15:40 +0000

    bump version to 0.2.0-pre.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk "0.2.0-SNAPSHOT"
+(defproject silk "0.2.0-pre.1"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
