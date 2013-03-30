ea7df604900754f8d7b75f09b30f641d47a9ac38
rossputin <rossajmcd@gmail.com>
2013-03-30 21:29:43 +0000

    add path transformations

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.taoensso/timbre "1.2.0"]]
+                 [com.taoensso/timbre "1.2.0"]
+                 [pathetic "0.4.0"]]
 
   :aot :all)
