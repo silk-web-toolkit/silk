b655631799e532c35b5184b31fe4efac8f3c61f5
rossputin <rossajmcd@gmail.com>
2013-04-14 20:12:18 +0100

    bump clojure dependency to 1.5.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject silk "0.2.0-pre.1"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.taoensso/timbre "1.2.0"]
                  [pathetic "0.4.0"]
                  [me.raynes/laser "0.1.22"]]
