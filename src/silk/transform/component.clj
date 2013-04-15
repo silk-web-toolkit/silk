1340808d3beb99cc36d186aa5ff3f5301934ffdf
rossputin <rossajmcd@gmail.com>
2013-04-15 20:14:38 +0100

    refactor, move component processing from silk-eden

--- src/silk/transform/component.clj
+++ src/silk/transform/component.clj
@@ -10,7 +10,7 @@
 ;; Component transformations, see namespace comment
 ;; =============================================================================
 
-(defn build-component
+(defn- build-component
   [i]
   (let [comp-str (str ((split i #":") 1) ".html")
         lcp (str se/pwd se/fs "components" se/fs comp-str)
@@ -18,3 +18,18 @@
         parsed-comp (l/parse c-path)]
     (l/select parsed-comp
               (l/child-of (l/element= :body) (l/any)))))
+
+(defn- component-inject
+  [c i]
+   (l/document
+     (l/parse c)
+       (l/id= i)
+       (l/replace (build-component i))))
+
+(defn process-components
+  [t]
+  (let [comps (l/select (l/parse (:content t)) (l/re-id #"silk-component"))
+        comp-ids (map #(:id (:attrs %)) comps)]
+    (assoc t :content
+      (first (as-> (:content t) page
+      (map #(component-inject page %) comp-ids))))))
