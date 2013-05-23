9de0bdb926f5e4492295bb2d910feeab3265cf49
rossputin <rossajmcd@gmail.com>
2013-05-23 22:35:44 +0100

    update timbre dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.taoensso/timbre "1.2.0"]
+                 [com.taoensso/timbre "1.3.0"]
                  [pathetic "0.4.0"]
                  [me.raynes/laser "0.1.22"]]
 
