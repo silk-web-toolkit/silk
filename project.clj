f54fa6105fa49e48735e1fbc555e5bc212bb9b85
Ross McDonald <Ross@rizzo.speed-trap.com>
2013-07-17 16:25:11 +0100

    #13 protect body class attributes via namespace

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk "0.2.1"
+(defproject silk "0.2.2"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
