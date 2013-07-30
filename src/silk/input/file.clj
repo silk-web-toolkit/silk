0776cdeb919df862c496a848cd06edd6e2515e9a
Ross McDonald <Ross@rizzo.speed-trap.com>
2013-07-30 17:23:04 +0100

    #23 migrate store-project-dir out of input in silk-core and into side effecting io of silk

--- src/silk/input/file.clj
+++ src/silk/input/file.clj
@@ -75,11 +75,3 @@
      (filter #(.isFile %))
      (map #(.getParent %))
      distinct))
-
-(defn store-project-dir
-  "Writes the current project directory to the central store."
-  []
-  (let [file se/spun-projects-file pwd se/pwd]
-    (if (not (.exists file)) (.createNewFile file))
-    (if (not (.contains (slurp (.getPath file)) pwd))
-      (spit (.getPath file) (str pwd "\n") :append true))))
