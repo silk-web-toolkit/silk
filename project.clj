2bb27f007acff63120fe050b72992ef8e7cad297
rossputin <rossajmcd@gmail.com>
2013-08-17 11:56:30 +0100

    set version to 0.3.0-pre.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk-core "0.3.0-pre.1+20130817"
+(defproject silk-core "0.3.0-pre.1"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
