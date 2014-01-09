b26d3cedbdb5abdf1579583fbeb7e32256ba1e7f
rossputin <rossajmcd@gmail.com>
2014-01-09 21:08:34 +0000

    bump version to 0.3.0-alpha.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk-core "0.3.0-pre.2"
+(defproject silk-core "0.3.0-alpha.1"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
