09872a39b76b579db3dd7ade57318aa68326735c
Ross McDonald <Ross.McDonald@passivsystems.com>
2014-07-15 09:23:31 +0100

    bump version to 0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk-core "0.4.1-SNAPSHOT"
+(defproject silk-core "0.5.0-SNAPSHOT"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
