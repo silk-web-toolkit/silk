2e918dd04679379780be0241154ab2693e508887
rossputin <rossajmcd@gmail.com>
2014-08-06 00:27:51 +0100

    bump version to 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject silk "0.5.0-SNAPSHOT"
+(defproject silk "0.5.0"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
