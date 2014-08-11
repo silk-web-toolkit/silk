3a7348f93261c93f84cb7e8929180b1180764f29
rossputin <rossajmcd@gmail.com>
2014-08-11 11:59:01 +0100

    update brogrammer installer and associated tgz build

--- build/build-tgz.sh
+++ build/build-tgz.sh
@@ -7,14 +7,10 @@
 # Ross McDonald <ross@bheap.co.uk>
 #
 
-if [ $1 != "" ]
-then
-    # Assumes that lein uberjar has been run and uberjar exists in target directory
-    mkdir -p ../target/tgz
-    cp ../target/*standalone* ../target/tgz/silk.jar
-    cp tgz/silk ../target/tgz
-    cd ../target/tgz
-    tar cvzf silk-$1.tgz * -C ../target/tgz
-else
-    echo "ERROR: VERSION variable not defined"
-fi
+# Assumes that lein uberjar has been run and uberjar exists in target directory
+mkdir -p ../target/tgz
+cp ../target/*standalone* ../target/tgz/silk.jar
+cp tgz/silk ../target/tgz
+cd ../target/tgz
+tar cvzf silk.tgz * -C ../target/tgz
+cd ../../build
