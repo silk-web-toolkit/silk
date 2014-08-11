df16d605767e627b47882c120fbc6b968d845a8f
rossputin <rossajmcd@gmail.com>
2014-08-11 11:34:36 +0100

    add brogrammer install

---
+++ install/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check me out before you run me, I'm just gonna install Silk honest
+
+# Borrowed from lein and emacs-live
+HTTP_CLIENT=${HTTP_CLIENT:-"wget -O"}
+if type -p curl >/dev/null 2>&1; then
+    HTTP_CLIENT="curl $CURL_PROXY -f -k -L"
+fi
+
+# Create user home bin directory if it does not exist
+if [ ! -d "$HOME/bin" ]; then
+  mkdir -p $HOME/bin
+fi
+
+function download_tarball {
+     echo ""
+     echo $(tput setaf 2)"--> Downloading Silk..."$(tput sgr0)
+     echo ""
+
+     $HTTP_CLIENT https://github.com/silk-web-toolkit/silk/releases/download/0.5.0/silk-0.5.0.tgz -o $HOME/bin/silk.tgz
+     tar xf $HOME/bin/silk.tgz -C $HOME/bin
+     rm $HOME/bin/silk.tgz
+}
+
+
+download_tarball
