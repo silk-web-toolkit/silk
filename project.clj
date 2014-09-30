08ff5d815ed46f00861a95d873456a5533ee053f
rossputin <rossajmcd@gmail.com>
2014-09-30 17:13:49 +0100

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.taoensso/timbre "1.5.2"]
-                 [pathetic "0.4.0"]
+                 [com.taoensso/timbre "3.3.1"]
+                 [pathetic "0.5.1"]
                  [me.raynes/laser "1.1.1"]
                  [me.rossputin/diskops "0.2.0"]
                  [org.clojars.zcaudate/watchtower "0.1.2"]
                  [io.aviso/pretty "0.1.12"]
-                 [org.apache.commons/commons-lang3 "3.1"]
+                 [org.apache.commons/commons-lang3 "3.3.2"]
                  [org.clojure/data.json "0.2.5"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
   :plugins [[lein-midje "3.1.0"]]
 
   :aot :all
