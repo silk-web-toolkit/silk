7b4a2fd925e5c88b228f6a0876899ef09fba1a7a
rossputin <rossajmcd@gmail.com>
2014-10-03 10:53:04 +0100

    add build monkey script

---
+++ build/build-bm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# build-tgz.sh Builds the protean tgz binary package for Build Monkey.
+# Must be run from the current directory as paths are relative.
+#
+# Ross McDonald <ross@bheap.co.uk>
+#
+
+# Assumes that lein uberjar has been run and uberjar exists in target directory
+mkdir -p ../target/bm
+cp ../target/*standalone* ../target/bm
+cp -r etc/* ../target/bm
+cd ../target/bm
+tar cvzf silk.tgz *
+cd ../../build
