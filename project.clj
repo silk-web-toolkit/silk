d25cfb9d2dfbcb13ce496927824cd814472eaf17
nick.featch@gmail.com <nick.featch@gmail.com>
2017-02-01 20:38:49 +0000

    bump to 0.8.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.silkyweb/silk "0.8.0"
+(defproject org.silkyweb/silk "0.8.1"
   :description "Silk static and dynamic publishing toolkit."
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
