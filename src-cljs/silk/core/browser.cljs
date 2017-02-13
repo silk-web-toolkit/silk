49483c1dc743ce00d49a31704be5fa03e2f0ce32
rossputin <rossajmcd@gmail.com>
2017-02-13 22:59:21 +0000

    tweak API to align with server side equivalent

--- src-cljs/silk/core/browser.cljs
+++ src-cljs/silk/core/browser.cljs
@@ -12,26 +12,26 @@
 (defn- hickorify [template]
   (first (hs/select (hs/child (hs/tag :body) hs/any) (h/as-hickory (h/parse (.-outerHTML template))))))
 
-(defn- elementById [id] (.getElementById js/document (name id)))
+(defn- elementById [tpl id] (.getElementById tpl (name id)))
 
-(defn- elements [] (.getElementsByTagName js/document "*"))
+(defn- elements [tpl] (.getElementsByTagName tpl "*"))
 
 (defn- elementsWithAttribute
-  [k]
-  (filter #(.getAttribute % (name k)) (cr/as-seq (elements))))
+  [tpl k]
+  (filter #(.getAttribute % (name k)) (cr/as-seq (elements tpl))))
 
 (defn spin-component-with-data
   "Splices data with component with given id"
-  [id data]
-  (let [template (elementById id)
+  [tpl id data]
+  (let [template (elementById tpl id)
         hick (hickorify template)
         res (cr/process-component-with-data hick data)]
     (set! (.-outerHTML template) (hr/hickory-to-html res))))
 
 (defn spin-components
   "Looks for data-sw-source and injects into it"
-  []
-  (doseq [el (elementsWithAttribute :data-sw-source)]
+  [tpl]
+  (doseq [el (elementsWithAttribute tpl :data-sw-source)]
     (go
       (let [hick (hickorify el)
             path (get-in hick [:attrs :data-sw-source])
